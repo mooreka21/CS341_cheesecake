83144144856eafd38c8a9f30601491fc652162ae
mooreka21 <mooreka21@up.edu>
2020-03-04 19:27:40 -0800

    orders.js added, app.js changed

---
+++ hw3/routes/orders.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET order listing. */
+router.get('/', function(req, res, next) {
+	var arr1 = { 
+		"data" : [ 
+			{ "toppings":"cherry",  "quantity":2 },
+			{ "toppings":"plain",  "quantity":6 },
+            { "toppings":"chocolate",  "quantity":3 },
+            	]
+	};
+	// turn arr1 into a string
+	var myJSON1 = JSON.stringify(arr1);
+	res.send(myJSON1);
+});
+
+module.exports = router;
